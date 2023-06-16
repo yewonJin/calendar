166df43ebca3f23125fbdb1fbfc1727fc1835876
yewonJin <jyw966@naver.com>
2023-06-16 17:07:44 +0900

    [UPDATE] 다음 월 버튼 기능

--- src/hooks/useMonth.test.ts
+++ src/hooks/useMonth.test.ts
@@ -1,22 +1,46 @@
-import { renderHook } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react";
 import useMonth from "./useMonth";
 
 describe("hooks 테스트", () => {
-  it("현재 년도 반환", () => {
-    const { result } = renderHook(() => useMonth());
+  const { result } = renderHook(() => useMonth());
 
+  it("현재 년도 반환", () => {
     expect(result.current.curYear).toBe(2023);
   });
 
   it("현재 월 반환", () => {
-    const { result } = renderHook(() => useMonth());
-
     expect(result.current.curMonth).toBe("JUN");
   });
-
-  it("next 버튼 클릭하면 다음 월 반환", () => {});
 
   it("prev 버튼 클릭하면 이전 월 반환", () => {});
 
   it("년도 선택 기능", () => {});
+});
+
+describe("next 버튼 클릭 했을 때", () => {
+  it("12월 아니면 월 + 1", () => {
+    const { result } = renderHook(() => useMonth());
+
+    act(() => {
+      result.current.setNextMonth();
+    });
+
+    expect(result.current.curMonth).toBe("JUL");
+  });
+
+  it("12월이면 년도 + 1, 1월로 설정", () => {
+    const { result } = renderHook(() => useMonth());
+
+    // 12월로 설정
+    act(() => {
+      result.current.setMonth("DEC");
+    });
+
+    act(() => {
+      result.current.setNextMonth();
+    });
+
+    expect(result.current.curMonth).toBe("JAN");
+    expect(result.current.curYear).toBe(2024);
+  });
 });
--- src/hooks/useMonth.ts
+++ src/hooks/useMonth.ts
@@ -1,10 +1,25 @@
 import { useState } from "react";
 
-const useMonth = () => {    
+const useMonth = () => {
   const [curMonth, setCurMonth] = useState(months[new Date().getMonth()]);
   const [curYear, setCurYear] = useState(new Date().getFullYear());
 
-  return { curMonth, curYear };
+  const setMonth = (month: (typeof months)[number]) => {
+    setCurMonth(month);
+  };
+
+  const setNextMonth = () => {
+    const curMonthIndex = months.indexOf(curMonth);
+
+    if (isLastMonth(curMonthIndex)) {
+      setCurMonth(months[0]);
+      setCurYear((prev) => prev + 1);
+    } else {
+      setCurMonth(months[curMonthIndex + 1]);
+    }
+  };
+
+  return { curMonth, curYear, setMonth, setNextMonth };
 };
 
 const months = [
@@ -20,6 +35,14 @@ const months = [
   "OCT",
   "NOV",
   "DEC",
-];
+] as const;
+
+const isLastMonth = (curMonthIndex: number) => {
+  if (curMonthIndex === months.length - 1) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 export default useMonth;
