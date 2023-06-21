6225da17de333cc55da8fe702ad4b55862e3771c
yewonJin <jyw966@naver.com>
2023-06-21 19:48:26 +0900

    [UPDATE] 캘린더 만드는 hooks

--- src/hooks/useCalendar.test.ts
+++ src/hooks/useCalendar.test.ts
@@ -1,46 +1,56 @@
 import { renderHook, act } from "@testing-library/react";
-import { useCalculateCalendar } from "./useCalendar";
+import useCalendar from "./useCalendar";
 
 describe("월의 시작일 설정", () => {
   // 토요일부터 0, 일요일 1, ... , 금요일 6
 
   it("2023년 6월은 목요일부터 1일", () => {
-    const { result } = renderHook(() => useCalculateCalendar("JUN", 2023));
+    const { result } = renderHook(() => useCalendar("JUN", 2023));
 
-    expect(result.current.setFirstDayOfMonth()).toBe(5);
+    expect(result.current.setFirstDayOfMonth()).toStrictEqual(5);
   });
 
-  it("2023년 1월은 일요일부터 1일", () => {
-    const { result } = renderHook(() => useCalculateCalendar("JAN", 2023));
+  it("2023년 9월은 금요일부터 1일", () => {
+    const { result } = renderHook(() => useCalendar("SEP", 2023));
 
-    expect(result.current.setFirstDayOfMonth()).toBe(1);
+    expect(result.current.setFirstDayOfMonth()).toStrictEqual(6);
   });
 
   it("2024년 3월은 금요일부터 1일", () => {
-    const { result } = renderHook(() => useCalculateCalendar("MAR", 2024));
+    const { result } = renderHook(() => useCalendar("MAR", 2024));
 
-    expect(result.current.setFirstDayOfMonth()).toBe(6);
+    expect(result.current.setFirstDayOfMonth()).toStrictEqual(6);
   });
 
   it("2026년 2월은 월요일부터 1일", () => {
-    const { result } = renderHook(() => useCalculateCalendar("FEB", 2026));
+    const { result } = renderHook(() => useCalendar("FEB", 2026));
 
-    expect(result.current.setFirstDayOfMonth()).toBe(1);
+    expect(result.current.setFirstDayOfMonth()).toStrictEqual(1);
   });
 
   it("2026년 1월은 목요일부터 1일", () => {
-    const { result } = renderHook(() => useCalculateCalendar("JAN", 2026));
+    const { result } = renderHook(() => useCalendar("JAN", 2026));
 
-    expect(result.current.setFirstDayOfMonth()).toBe(5);
+    expect(result.current.setFirstDayOfMonth()).toStrictEqual(5);
   });
 
   it("2029년 8월은 수요일부터 1일", () => {
-    const { result } = renderHook(() => useCalculateCalendar("AUG", 2029));
+    const { result } = renderHook(() => useCalendar("AUG", 2029));
 
-    expect(result.current.setFirstDayOfMonth()).toBe(4);
+    expect(result.current.setFirstDayOfMonth()).toStrictEqual(4);
   });
 });
 
-describe("월의 종료일 설정", () => {
-  it("ex) 2023년 6월은 금요일이 30일", () => {});
+describe("월의 일수", () => {
+  it("ex) 2023년 6월은 30일까지 있다.", () => {
+    const { result } = renderHook(() => useCalendar("JUN", 2023));
+
+    expect(result.current.numberOfDaysOfCurMonth).toBe(30);
+  });
+
+  it("2024년 2월은 29일까지 있다.", () => {
+    const { result } = renderHook(() => useCalendar("FEB", 2024));
+
+    expect(result.current.numberOfDaysOfCurMonth).toBe(29);
+  });
 });
--- src/hooks/useCalendar.ts
+++ src/hooks/useCalendar.ts
@@ -1,13 +1,9 @@
 import { Months } from "../types/month";
 import { months } from "./useMonth";
 
-export const useRenderCalendar = (
-  firstDayOfMonth: number,
-  lastDayOfMonth: number
-) => {};
-
 // baseYear: 2022, baseMonth: 1, baseDay: 1, baseDayOfTheWeek: "SAT";
-export const useCalculateCalendar = (curMonth: Months, curYear: number) => {
+const useCalendar = (curMonth: Months, curYear: number) => {
+  /** 월의 시작일 설정하는 함수 */
   const setFirstDayOfMonth = () => {
     const curMonthIndex = months.indexOf(curMonth);
 
@@ -23,30 +19,34 @@ export const useCalculateCalendar = (curMonth: Months, curYear: number) => {
     );
   };
 
-  const setLastDayOfMonth = () => {};
+  /** 현재 월의 일수 */
+  const numberOfDaysOfCurMonth =
+    curYear % 4 === 0 && curMonth === "FEB"
+      ? numberOfDaysPerMonth[months.indexOf(curMonth)] + 1
+      : numberOfDaysPerMonth[months.indexOf(curMonth)];
 
-  return { setFirstDayOfMonth };
+  const startIndex = setFirstDayOfMonth() === 0 ? 7 : setFirstDayOfMonth();
+
+  const endIndex = startIndex + numberOfDaysOfCurMonth;
+  return { startIndex, endIndex, setFirstDayOfMonth, numberOfDaysOfCurMonth };
 };
 
-const numberOfDaysPerMonth = [
-  31, 28, 31, 30, 31, 30, 31, 30, 31, 31, 31, 30, 31,
-];
-
-const baseYear = 2022;
+/** 월별 일수 */
+const numberOfDaysPerMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 const yearGap = (curYear: number) => (curYear - baseYear) * 365;
 
+/** 윤달 계산 알고리즘 */
 const isThereLeapMonth = (curYear: number, curMonthIndex: number) => {
-  // 현재 년도가 윤년이고 3월 이상이면 그 횟수만큼 + 1
+  // 현재 년도가 윤년이고 3월 이상이면 그 횟수만큼 + 1일
   if (curYear % 4 === 0) {
     if (curMonthIndex >= 2) return Math.floor((curYear - baseYear) / 4) + 1;
     else return Math.floor((curYear - baseYear) / 4);
   }
 
-  
-  // 현재 년도와 baseYear 사이에 윤년이 껴있으면 그 횟수만큼 +1
+  // 현재 년도와 baseYear 사이에 윤년이 껴있으면 그 횟수만큼 +1일
   let sum = 0;
-  
+
   for (let i = baseYear; i <= curYear; i++) {
     if (i % 4 === 0) {
       sum++;
@@ -55,3 +55,7 @@ const isThereLeapMonth = (curYear: number, curMonthIndex: number) => {
 
   return sum;
 };
+
+const baseYear = 2022;
+
+export default useCalendar;
